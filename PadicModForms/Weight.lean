26fcbe8ba28a00dea630fda540b7b20bcfd34411
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-05-24 13:39:18 +0200

    this is AI going too far removing warnings

--- PadicModForms/Weight.lean
+++ PadicModForms/Weight.lean
@@ -20,12 +20,14 @@ open PowerSeries Filter Topology
 
 variable {p : ℕ} [Fact p.Prime] (n : ℕ)
 
-axiom w_tendsto {f : ℚ_[p]⟦X⟧} (hf : f ≠ 0) (S : pAdicModularFormStruct n f) :
-    ∃ (x : X_[p]), Tendsto (fun i ↦ ι (S.w i)) atTop (𝓝 x)
+theorem w_tendsto {f : ℚ_[p]⟦X⟧} (hf : f ≠ 0) (S : pAdicModularFormStruct n f) :
+    ∃ (x : X_[p]), Tendsto (fun i ↦ ι (S.w i)) atTop (𝓝 x) := by
+  sorry
 
-axiom limit_unique {f : ℚ_[p]⟦X⟧} (S S' : pAdicModularFormStruct n f) {x x' : X_[p]}
+theorem limit_unique {f : ℚ_[p]⟦X⟧} (S S' : pAdicModularFormStruct n f) {x x' : X_[p]}
     (hx : Tendsto (fun i ↦ ι (S.w i)) atTop (𝓝 x))
-    (hx' : Tendsto (fun i ↦ ι (S'.w i)) atTop (𝓝 x')) : x = x'
+    (hx' : Tendsto (fun i ↦ ι (S'.w i)) atTop (𝓝 x')) : x = x' := by
+  sorry
 
 open Classical in
 noncomputable def w {f : ℚ_[p]⟦X⟧} (hf : f.isPAdicModularForm n) : X_[p] :=
