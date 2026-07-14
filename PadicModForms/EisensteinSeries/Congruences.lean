18413c579f5d5b8df0e1f3ade2ee976ec131f855
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-07-14 16:27:07 +0800

    add E_int_mod_p

--- PadicModForms/EisensteinSeries/Congruences.lean
+++ PadicModForms/EisensteinSeries/Congruences.lean
@@ -12,8 +12,8 @@ import PadicModForms.ForMathlib.Bernoulli
 /-!
 # Congruences for Eisenstein series
 
-This file proves that the normalized Eisenstein series of weight `p - 1` is congruent to one
-modulo `p`.
+This file proves that a normalized Eisenstein series whose weight is divisible by `p - 1` is
+congruent to one modulo `p`.
 -/
 
 @[expose] public section
@@ -26,18 +26,21 @@ namespace EisensteinSeries
 
 open ModP
 
-/-- The normalized Eisenstein series of weight `p - 1` is congruent to `1` modulo `p`. -/
-theorem E_p_sub_one_mod_p (hp : 5 ≤ p) : (E hp).map pLocalInt.toZMod = 1 := by
-  let hk : 3 ≤ p - 1 := by lia
-  let hk2 : Even (p - 1) := Hp.out.even_sub_one (by lia)
-  let B_i : pLocalInt p := ⟨(bernoulli _)⁻¹, inv_bernoulli_mem_pLocalInt hk hk2 dvd_rfl⟩
-  have hB_inv : pLocalInt.toZMod B_i = 0 := toZMod_inv_bernoulli_eq_zero hk hk2 dvd_rfl
+/-- If `p - 1 ∣ k`, then the normalized Eisenstein series of weight `k` is congruent to `1`
+modulo `p`. -/
+theorem E_int_mod_p {k : ℕ} (hk : 3 ≤ k) (hk2 : Even k) (hpk : p - 1 ∣ k) :
+    (E_int hk hk2 hpk).map pLocalInt.toZMod = 1 := by
+  let B_i : pLocalInt p := ⟨(bernoulli k)⁻¹, inv_bernoulli_mem_pLocalInt hk hk2 hpk⟩
+  have hB_inv : pLocalInt.toZMod B_i = 0 := toZMod_inv_bernoulli_eq_zero hk hk2 hpk
   ext n
   by_cases hn : n = 0
-  · simp [hn, coeff_E_int_zero hk hk2 dvd_rfl]
-  have hcoeff : coeff n (E hp) = -(2 * (p - 1)) * B_i * σ (p - 2) n := by
-      convert coeff_E_int_of_ne_zero hk hk2 dvd_rfl hn using 1
-      simp [show 1 ≤ p by lia, show p - 1 - 1 = p - 2 by lia, B_i]
+  · simp [hn, coeff_E_int_zero hk hk2 hpk]
+  have hcoeff : coeff n (E_int hk hk2 hpk) = -(2 * k) * B_i * σ (k - 1) n := by
+    simpa [B_i] using coeff_E_int_of_ne_zero hk hk2 hpk hn
   simp [hn, hcoeff, map_mul, map_mul, map_neg, hB_inv]
+
+/-- The normalized Eisenstein series of weight `p - 1` is congruent to `1` modulo `p`. -/
+theorem E_p_sub_one_mod_p (hp : 5 ≤ p) : (E hp).map pLocalInt.toZMod = 1 := by
+  exact E_int_mod_p (by lia) (Hp.out.even_sub_one (by lia)) dvd_rfl
 
 end EisensteinSeries
