410dfb6cdaaf4dc26ecadae427759cfeca2b942b
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-08-04 18:04:57 +0200

    add file

--- PadicModForms.lean
+++ PadicModForms.lean
@@ -28,6 +28,7 @@ import PadicModForms.Rational.E2
 import PadicModForms.Rational.Eisenstein
 import PadicModForms.Rational.Graded
 import PadicModForms.Rational.Ramanujan
+import PadicModForms.Rational.SerreDerivative
 import PadicModForms.SpherePacking.Bridge
 import PadicModForms.pLocalInt.Basic
 import PadicModForms.pLocalInt.Discriminant
